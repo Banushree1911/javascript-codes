083bdc7bff1e0cc4ff5b4337439b45bfdbfeedab
Banushree1911 <banushreebanushree@gmail.com>
2024-08-12 15:31:01 +0530

    Add files via upload

---
+++ vowels_string.js
@@ -0,0 +1,15 @@
+function countVowels(str) {
+    const vowels = "aeiouAEIOU";
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Example usage:
+const inputString = "Hello World";
+const vowelCount = countVowels(inputString);
+console.log(The number of vowels in the string is: ${vowelCount});
