687dbaddad8e31a6a3fda1b9294da0775dc99f08
Banushree1911 <banushreebanushree@gmail.com>
2024-08-23 21:07:45 +0530

    Add files via upload

---
+++ reverse_string.js
@@ -0,0 +1,15 @@
+let s = reverseString("JavaScript");
+println(s);
+
+function reverseString(s)
+{
+    let s2 = "";
+    
+    for(let i = s.length - 1; i >= 0; i--)
+    {
+        let char = s[i];
+        s2 += char;
+    }
+    
+    return s2;
+}
